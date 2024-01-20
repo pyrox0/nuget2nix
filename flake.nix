5a383b9fb0113b8bdf4fb2865265faf768009fdd
Adam Woods <squirmy.dev@gmail.com>
2024-01-20 16:52:01 +1100

    chore: update rust toolchain

--- flake.nix
+++ flake.nix
@@ -1,48 +1,80 @@
 {
-  description = "Generates a Nix expression for `buildDotnetModule`";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
+  outputs = inputs @ {
+    flake-parts,
+    fenix,
+    ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib stdenv;
-      in {
-        packages.nuget2nix = pkgs.rustPlatform.buildRustPackage {
-          pname = "nuget2nix";
-          version = (lib.importTOML ./Cargo.toml).package.version;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+        {
+          perSystem = {
+            pkgs,
+            system,
+            config,
+            ...
+          }: let
+            inherit (pkgs) lib stdenv;
+            toolchain = pkgs.fenix.minimal.toolchain;
+          in {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [fenix.overlays.default];
+            };
 
-          src = self;
-          cargoLock.lockFile = ./Cargo.lock;
+            packages = rec {
+              nuget2nix =
+                (pkgs.makeRustPlatform {
+                  cargo = toolchain;
+                  rustc = toolchain;
+                })
+                .buildRustPackage {
+                  pname = "nuget2nix";
+                  version = (lib.importTOML ./Cargo.toml).package.version;
+                  src = ./.;
+                  cargoLock.lockFile = ./Cargo.lock;
 
-          buildInputs = lib.optional stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
-        };
-        defaultPackage = self.packages.${system}.nuget2nix;
+                  buildInputs = lib.optional stdenv.isDarwin [
+                    pkgs.darwin.apple_sdk.frameworks.Security
+                    pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+                  ];
+                };
 
-        apps.nuget2nix = {
-          type = "app";
-          program = "${self.packages.${system}.nuget2nix}/bin/nuget2nix";
-        };
-        defaultApp = self.apps.${system}.nuget2nix;
+              default = nuget2nix;
+            };
 
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs;
-            [rustc cargo clippy]
-            ++ lib.optionals stdenv.isDarwin [
-              libiconv
-              darwin.apple_sdk.frameworks.Security
-              darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-        };
-      }
-    );
+            apps = rec {
+              nuget2nix = {
+                type = "app";
+                program = "${config.packages.nuget2nix}/bin/nuget2nix";
+              };
+              default = nuget2nix;
+            };
+
+            devshells.default = {
+              packages = [
+                (pkgs.fenix.complete.withComponents [
+                  "cargo"
+                  "clippy"
+                  "rust-src"
+                  "rustc"
+                  "rustfmt"
+                ])
+                pkgs.rust-analyzer-nightly
+              ];
+            };
+          };
+        }
+      ];
+      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+    };
 }
